4e02b082f9d8f7a3cc5d6bcd0ef5a231627e8a12
SantiagoT117 <santiago.torres.rojas@correounivalle.edu.co>
2025-10-12 18:40:55 -0500

    Primer commit miniproyecto

---
+++ src/The_Hero.java
@@ -0,0 +1,22 @@
+public class The_Hero extends Characters{
+
+    public The_Hero(String name,int hP, int mP, int atack, int defense, int speed, float crit_Damage, int luck, float precision,
+            int wisdom, String skills){
+        super(name, hP, mP, atack, defense, speed, crit_Damage, luck, precision, wisdom, skills);
+    }
+
+    public String toString(){
+        return getName() + 
+        "/ HP " + getHP() +
+        "/ MP " + getMP() +
+        "/ Atack " + getAtack() +
+        "/ Defense " + getDefense() +
+        "/ Speed " + getSpeed() +
+        "/ Crit Damage " + getCrit_Damage() +
+        "/ Luck " + getLuck() +
+        "/ Precision " + getPrecision() + 
+        "/ Wisdom " + getWisdom() +
+        "/ Abilities " + getSkills() + "\n";
+    }
+
+}
