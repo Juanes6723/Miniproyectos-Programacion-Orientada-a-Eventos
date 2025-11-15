f268796cb0c43c312e758a3d3b6991e43767b1c2
JosueCocoma <josue.cocoma@correounivalle.edu.co>
2025-11-15 12:59:36 -0500

    Inicio de implementacion del modelo MVC con modelo

---
+++ Miniproyecto3/src/model/SkillsList.java
@@ -0,0 +1,6 @@
+public enum SkillsList {
+    SWEET_BREATH, //Habilidades, se pueden agregar mas para despues
+    FIREBALL,
+    HEAL,
+    DRAGON_FIRE
+}
---
+++ Miniproyecto3/src/model/TypeEnemy.java
@@ -0,0 +1,7 @@
+public enum TypeEnemy {
+    AGGRESSIVE, //Tipos de enemigos, se pueden agregar mas despues
+    HEALER, 
+    DEFENSIVE, 
+    SORCERER,
+    MINI_BOSS
+}
